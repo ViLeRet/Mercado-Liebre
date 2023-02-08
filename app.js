e3f93705514c74de0c5aa7b76060c94688139584
ViLeRet <mzb.988@gmail.com>
2023-02-07 22:28:58 -0300

    archivos

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicFolderPath = path.resolve(__dirname, './public');
+app.use(express.static(publicFolderPath));
+
+app.listen(3000, () => { console.log('Servidor corriendo en el puerto 3000') });
+
+app.get('/', (req, res) => { 
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+ });
